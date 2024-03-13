efd35326718c7d4023c24da01d1df5e0949d45f7
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-12 22:11:59 -0300

    feat: add search params

--- src/car_dealership_api/views/vehicle_view.py
+++ src/car_dealership_api/views/vehicle_view.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from ..serializers.vehicle_serializer import VehicleSerializer
 from ..models.vehicle import Vehicle
 
@@ -6,8 +7,11 @@ from ..models.vehicle import Vehicle
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     http_method_names = ["get", "post", "retrieve", "put", "patch"]
-    ordering_fields = ["model", "year", "price"]
+    ordering_fields = ["model", "year", "price"]  # ordering
+    filterset_fields = ["vehicle_type", "brand", "year"]
+    search_fields = ["model"]  # search
 
     def get_queryset(self):
         queryset = super().get_queryset()
